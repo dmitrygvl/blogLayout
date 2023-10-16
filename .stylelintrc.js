7fbd753bb7e66789507e95df2ff49ac54517ae0d
dmitrygvl <dmitrygv1994@ya.ru>
2023-10-16 19:56:08 +0300

    Webpack install

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ["stylelint-config-standard"],
-  plugins: ["stylelint-prettier"],
+  plugins: ["stylelint-prettier", "stylelint-scss"],
   rules: {
     "prettier/prettier": true,
   },
