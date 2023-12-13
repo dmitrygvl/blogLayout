009e8b2569428cd2c627d86003673022533a7bc7
dmitrygvl <dmitrygv1994@ya.ru>
2023-12-13 17:05:10 +0300

    Fix formatting

--- src/modules/sliderLogic.js
+++ src/modules/sliderLogic.js
@@ -104,9 +104,3 @@ export default class Slider {
     return button;
   }
 }
-
-export function sliderInit() {
-  const options = { gap: "7%", duration: "1s", interval: "10000" };
-  const carousel = new Slider(document.querySelector(".slider"), options);
-  carousel.initialize();
-}
